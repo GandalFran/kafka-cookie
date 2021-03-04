cfa4837917fe0841327ef344d6c4c88c3e394235
gandalfran <franpintosantos@usal.es>
2021-03-04 14:20:02 +0100

    .

--- {{cookiecutter.name}}/{{cookiecutter.name}}/processing/data_processor.py
+++ {{cookiecutter.name}}/{{cookiecutter.name}}/processing/data_processor.py
@@ -1,12 +1,14 @@
 # Copyright {% now 'local', '%Y' %} {{ cookiecutter.author }}
 # See LICENSE for details.
 
+from time import sleep
 from datetime import datetime
 
 class DataProcessor:
 
 	def process(self, data=None):
 		if data is None:
+			sleep(1)
 			data = datetime.now().isoformat()
 
 		print(f'processing in {{ cookiecutter.name }}: {data}')
