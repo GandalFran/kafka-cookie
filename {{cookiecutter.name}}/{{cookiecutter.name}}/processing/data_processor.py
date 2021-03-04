d5d0916eaa926573ba4b823927e6083c3f77c39b
gandalfran <franpintosantos@usal.es>
2021-03-04 14:18:09 +0100

    .

--- {{cookiecutter.name}}/{{cookiecutter.name}}/processing/data_processor.py
+++ {{cookiecutter.name}}/{{cookiecutter.name}}/processing/data_processor.py
@@ -1,8 +1,13 @@
 # Copyright {% now 'local', '%Y' %} {{ cookiecutter.author }}
 # See LICENSE for details.
 
+from datetime import datetime
+
 class DataProcessor:
 
 	def process(self, data=None):
+		if data is None:
+			data = datetime.now().isoformat()
+
 		print(f'processing in {{ cookiecutter.name }}: {data}')
 		return data
